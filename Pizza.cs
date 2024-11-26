2c9103bb3b39e7763936ab83268f7662fd7f02ce
imyya <126475223+imyya@users.noreply.github.com>
2024-11-26 10:35:02 +0000

    creation model Pizza

---
+++ Pizza.cs
@@ -0,0 +1,8 @@
+namespace PizzaStore.Models 
+{
+    public class PizzaEhod {
+        public int IdEhod { get; set;}
+        public string? NomEhod { get; set;}
+        public string? DescriptionEhod { get; set;}
+    }
+}
